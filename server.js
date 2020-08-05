4d423bedc9eca2fe8486050bcd79601caa5f1cf5
Youssef Khedher <khedher.youssef@hotmail.fr>
2020-08-05 22:17:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
+// const cors = require('cors')
+// app.use(cors())
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
 const { v4: uuidV4 } = require('uuid')
+
+app.use('/peerjs', peerServer);
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -18,7 +26,12 @@ app.get('/:room', (req, res) => {
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
     socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId)
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+    // messages
+    socket.on('message', (message) => {
+      //send message to the same room
+      io.to(roomId).emit('createMessage', message)
+  }); 
 
     socket.on('disconnect', () => {
       socket.to(roomId).broadcast.emit('user-disconnected', userId)
@@ -26,4 +39,4 @@ io.on('connection', socket => {
   })
 })
 
-server.listen(3000)
+server.listen(process.env.PORT||3030)
