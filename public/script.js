0316f30867cfd9a1fb94aa9943a6b961dac649f2
Jinoy Varghese <68021115+Jinoy-Varghese@users.noreply.github.com>
2021-02-08 10:34:41 +0530

    msg

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   path: '/peerjs',
   host: '/',
-  port: '3030'
+  port: '443'
 })
 let myVideoStream;
 const myVideo = document.createElement('video')
