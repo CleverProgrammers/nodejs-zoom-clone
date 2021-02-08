f6aaed6328a1664be23257c939353a7879182724
Jinoy Varghese <68021115+Jinoy-Varghese@users.noreply.github.com>
2021-02-08 21:49:07 +0530

    msg

--- public/script.js
+++ public/script.js
@@ -1,7 +1,6 @@
 
 
-function videoshare()
-{
+
 
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
@@ -189,39 +188,8 @@ const setPlayVideo = () => {
 
 
 
-}
 
-const setMuteButton = () => {
-  const html = `
-    <i class="fas fa-microphone"></i>
-    <span>Mute</span>
-  `
-  document.querySelector('.main__mute_button').innerHTML = html;
-}
 
-const setUnmuteButton = () => {
-  const html = `
-    <i class="unmute fas fa-microphone-slash"></i>
-    <span>Unmute</span>
-  `
-  document.querySelector('.main__mute_button').innerHTML = html;
-}
-
-const setStopVideo = () => {
-  const html = `
-    <i class="fas fa-video"></i>
-    <span>Stop Video</span>
-  `
-  document.querySelector('.main__video_button').innerHTML = html;
-}
-
-const setPlayVideo = () => {
-  const html = `
-  <i class="stop fas fa-video-slash"></i>
-    <span>Play Video</span>
-  `
-  document.querySelector('.main__video_button').innerHTML = html;
-}
 function shareScreen()
 {
 
