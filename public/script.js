c4366b0f21655b44b5224a431be9963171c9f2d0
Jinoy Varghese <68021115+Jinoy-Varghese@users.noreply.github.com>
2021-02-09 09:45:32 +0530

    msg

--- public/script.js
+++ public/script.js
@@ -235,7 +235,7 @@ function connectToNewUser(userId, stream) {
   const call = myPeer.call(userId, stream)
   const video = document.createElement('video')
   call.on('stream', userVideoStream => {
-    addVideoStream(video, userVideoStream)
+    
   })
   call.on('close', () => {
     video.remove()
