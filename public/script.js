735b3291b96d89fb7321af4a3f228cba946c7b5e
Jinoy Varghese <68021115+Jinoy-Varghese@users.noreply.github.com>
2021-02-09 09:25:56 +0530

    msg

--- public/script.js
+++ public/script.js
@@ -215,45 +215,14 @@ navigator.mediaDevices.getDisplayMedia({
     })
   })
 
-  socket.on('user-connected', userId => {
-    connectToNewUser(userId, stream)
-  })
-  // input value
-  let text = $("input");
-  // when press enter send message
-  $('html').keydown(function (e) {
-    if (e.which == 13 && text.val().length !== 0) {
-      socket.emit('message', text.val());
-      text.val('')
-    }
-  });
-  socket.on("createMessage", message => {
-    $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
-    scrollToBottom()
-  })
+
 })
 
-socket.on('user-disconnected', userId => {
-  if (peers[userId]) peers[userId].close()
-})
+
 
 myPeer.on('open', id => {
   socket.emit('join-room', ROOM_ID, id)
 })
-
-function connectToNewUser(userId, stream) {
-  
-  const call = myPeer.call(userId, stream)
-  const video = document.createElement('video')
-  call.on('stream', userVideoStream => {
-    addVideoStream(video, userVideoStream)
-  })
-  call.on('close', () => {
-    video.remove()
-  })
-
-  peers[userId] = call
-}
 
 function addVideoStream(video, stream) {
   video.srcObject = stream
@@ -262,9 +231,5 @@ function addVideoStream(video, stream) {
   })
   videoGrid.append(video)
 }
-
-
-
-
 
 }
